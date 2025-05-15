3aa2599a9e4a378f4bce7460a31afc35fd5e899a
giacomo_cicala <giacomo.cicala@studio.unibo.it>
2025-05-15 19:02:20 +0200

    _

---
+++ root/valori_attesi.cpp
@@ -0,0 +1,13 @@
+
+double RW{3293};
+double LW{47.2E-3};
+double RLW{120};
+
+double RM{3287};
+double LM{46.9E-3};
+double CM{4.76E-9};
+double RLM{120.7};
+
+double RT{3295};
+double CT{4.76E-9};
+
